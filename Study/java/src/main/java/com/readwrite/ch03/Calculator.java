615a2f70327d6d716818b2b8a595e4bc1f4158a0
yusoyoung <soyong92@gmail.com>
2016-08-14 11:48:32 +0900

    test

---
+++ Study/java/src/main/java/com/readwrite/ch03/Calculator.java
@@ -0,0 +1,23 @@
+package com.readwrite.ch03;
+
+/**
+ * Created by 유소영 on 2016-07-31.
+ */
+public class Calculator {
+
+    public long add(int a, int b){
+        return (long)a+b;
+    }
+
+    public long multiply(int c, int d){
+        return (long)c*d;
+    }
+
+    public long divide(int e, int f){
+        return (long)e/f;
+    }
+
+    public long substract(int g, int h){
+        return (long)g-h;
+    }
+}
